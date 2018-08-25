06a2dad6925b124063202dc40f8a0ff4d9a98c56
JakeComing <jake@d4webdesign.com>
2018-08-25 02:11:17 -0700

    validated that total width of hex row can be calculated given width of a single hexagon and the desired number of hexagons

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
+// creates the HTML & CSS for a single hexagon.
 function createSingleHexagon(hexAttr, positionInRow) {
 
 }
+
+// responsible for assembling html output and assigning styles
 function constructHexagonRow(rowAttributes) {
   var singleHexWidth = rowAttributes.singleWidth;
   var hexCount = rowAttributes.hexCount;
   var markupToInsert = '';
   var hexRows = document.getElementsByClassName("hex-row");
-  var totalRequestedWidth = hexCount * singleWidth;
-  if (totalRequestedWidth < window.innerWidth;) {
+  var totalRequestedWidth = hexCount * singleHexWidth;
+  console.log("the totalRequestedWidth is: " + totalRequestedWidth);
+  if (totalRequestedWidth < window.innerWidth) {
     console.log("this is a reasonable request");
   } else {
     console.log("If I render this, you won't be able to see the whole thing!");
   }
   //  return markupToInsert;
-}
-/*
-Example: constructHexagonRow({
-  singleWidth: 200
-  hexCount: 5
-  align: center
-});
+} // constructHexagonRow()
+
+// Example: constructHexagonRow({
+//   singleWidth: 200
+//   hexCount: 5
+//   align: center
+// });
 
 
 
