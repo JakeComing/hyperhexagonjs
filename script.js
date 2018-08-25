f88b922ab1dd76a122f485689d911d69ee497eea
JakeComing <jake@d4webdesign.com>
2018-08-25 01:52:47 -0700

    define local hexagon row variables and start conditionally classifying function request

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
+function createSingleHexagon(hexAttr, positionInRow) {
 
-function constructHexagonRow() {
-    var markupToInsert = '';
-    var hexRows = document.getElementsByClassName("hex-row");
-    var windowWidth = window.innerWidth;
-    console.log("The window width is: "+windowWidth);
-    return markupToInsert;
 }
-constructHexagonRow();
-// document.getElementById("first-row").innerHTML = constructHexagonRow();
+function constructHexagonRow(rowAttributes) {
+  var singleHexWidth = rowAttributes.singleWidth;
+  var hexCount = rowAttributes.hexCount;
+  var markupToInsert = '';
+  var hexRows = document.getElementsByClassName("hex-row");
+  var totalRequestedWidth = hexCount * singleWidth;
+  if (totalRequestedWidth < window.innerWidth;) {
+    console.log("this is a reasonable request");
+  } else {
+    console.log("If I render this, you won't be able to see the whole thing!");
+  }
+  //  return markupToInsert;
+}
+/*
+Example: constructHexagonRow({
+  singleWidth: 200
+  hexCount: 5
+  align: center
+});
+
+
 
 // Do this after document loads
 $(document).ready(function(){
