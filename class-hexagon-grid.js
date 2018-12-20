0a3133ac2b1afa828c656573f2d2732814ad0108
JakeComing <jake@d4webdesign.com>
2018-12-20 00:24:56 -0800

    Create hexagonGrid class with constructor

---
+++ class-hexagon-grid.js
@@ -0,0 +1,7 @@
+class hexagonGrid {
+  constructor(gridHeight, gridWidth, hexagonDimensions) {
+    this.gridHeight = gridHeight;
+    this.gridWidth = gridWidth;
+    this.hexagonDimensions = hexagonDimensions;
+  }
+}
